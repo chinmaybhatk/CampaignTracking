ac845fa23dd439c77eb103933b3eabcfacf270e3
chinmaybhatk <chinmaybhatk@gmail.com>
2025-06-10 14:14:17 +0530

    Create hooks for Campaign Tracking

--- campaign_tracking/hooks.py
+++ campaign_tracking/hooks.py
@@ -2,53 +2,20 @@ from . import __version__ as app_version
 
 app_name = "campaign_tracking"
 app_title = "Campaign Tracking"
-app_publisher = "Chinmay Bhatk"
+app_publisher = "Chinmay Bhat"
 app_description = "Campaign and UTM Link Tracking"
-app_email = "chinmay@example.com"
-app_license = "MIT"
 app_version = app_version
+app_email = "chinmaybhatk@gmail.com"
+app_license = "MIT"
 
-# Includes in <head>
-app_include_css = "/assets/campaign_tracking/css/campaign_tracking.css"
-app_include_js = "/assets/campaign_tracking/js/campaign_tracking.js"
+before_install = "campaign_tracking.utils.install.before_install"
+after_install = "campaign_tracking.utils.install.after_install"
 
-# DocTypes for fixtures
 fixtures = [
     {
         "dt": "DocType", 
         "filters": [
-            ["name", "in", ["UTM Link", "Click Tracking"]]
+            ["name", "in", ["UTM Link"]]
         ]
     }
 ]
-
-# Web Routes
-website_route_rules = [
-    {"name": "utm_redirect", "page_name": "utm_redirect", "generator": "UTM Link"}
-]
-
-# Permissions
-permission_query_conditions = {
-    "UTM Link": "campaign_tracking.utils.permissions.get_permission_query_conditions",
-    "Click Tracking": "campaign_tracking.utils.permissions.get_permission_query_conditions"
-}
-
-# Dashboard configurations
-dashboards = {
-    "UTM Link": "campaign_tracking/doctype/utm_link/utm_link_dashboard.py"
-}
-
-# Scheduler Tasks
-scheduler_events = {
-    "daily": [
-        "campaign_tracking.utils.analytics.cleanup_old_tracking_data"
-    ]
-}
-
-# Customizations and Extensions
-override_doctype_class = {
-    "UTM Link": "campaign_tracking.controllers.utm_link.CustomUTMLink"
-}
-
-# Notification configurations
-notification_config = "campaign_tracking.utils.notifications.get_notification_config"
